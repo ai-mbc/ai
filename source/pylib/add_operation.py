2e99e21d25e796ca6bd9e5a530fd06146af07128
watermelonkim0704-star <watermelonkim0704@gmail.com>
2026-08-04 18:01:53 +0900

    JAVAsc

--- source/pylib/add_operation.py
+++ source/pylib/add_operation.py
@@ -11,21 +11,40 @@ def add_return(n):
     for i in range(1, n+1):
         tot += i # tot = tot+i
     return tot
-PI=3.141592
+PI = 3.141592
 '''
     터미널에서 python add_operation.py 5
     인터프리터 선택 : ctrl+shift+p => select interpreter => (base)선택
     터미널 띄우기 : ctrl+j
 '''
-if __name__=='__main__':
-   import sys
-   print(sys.argv)
-   if len(sys.argv)>1:
-       var=int(sys.argv[1])
-   else:
-       print('기본적으로 실행시 숫자를 입력하지 않으면 100으로')
-       var=100
-   print('1.add_print',end='')
-   add_print(var)
-   print('2.add_return :',add_return(var))
-   print(PI)
+if __name__=="__main__":
+    import sys
+    #print(sys.argv)
+    if len(sys.argv)>1:
+        var = int(sys.argv[1])
+    else:
+        print('기본적으로 실행시 숫자를 입력하지 않으면 100으로')
+        var = 100
+    print('1. add_print', end='')
+    add_print(var)
+    print('2. add_return :', add_return(var))
+    print(PI)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- source/pylib/sample_pac/ab/b.py
+++ source/pylib/sample_pac/ab/b.py
@@ -1,3 +1,2 @@
-#sample_pac/ac/b.py
 def world():
     print('sample_pac/ab/b모듈안의 world')
