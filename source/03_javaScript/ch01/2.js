e3e3bf47343ff7ace6be0d8fe3d5e54cd3c1f1b1
watermelonkim0704-star <watermelonkim0704@gmail.com>
2026-08-05 18:00:38 +0900

    반복문

--- source/03_javaScript/ch01/2.js
+++ source/03_javaScript/ch01/2.js
@@ -1,8 +1,9 @@
-/*변수선언시 var(전역변수),let(지역변수),const(상수)*/
-let sum =0;
-for (var i=1; i<=5; i++){
-  sum+=i; //sum=sum+i
-  console.log('i=',i,'일때까지 누적돈 합은',sum);
+/* 변수 선언시 var(전역변수), let(지역변수), const(상수) */
+let sum = 0;
+// for(초기화 ; 조건 ; 증감){ 반복문들 }
+for (var i=1 ; i<=5 ; i++){
+  sum += i; //sum = sum + i;
+  console.log('i=', i, '일때까지 누적된 합은', sum);
 }
-console.log('for문끝')
+console.log('for문 끝')
 console.log('for문 끝난 후 i값은 '+i);
